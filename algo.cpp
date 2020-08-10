20b2ae9788e025f854373814048b76577d3d319b
DATasso <50896863+DATasso@users.noreply.github.com>
2020-08-10 17:42:25 -0400

    Todo funcional, falta arreglar error que pierde height%procesadores lineas

--- algo.cpp
+++ algo.cpp
@@ -3,6 +3,9 @@
 
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string.h>
 #include "mpi.h"
 
 using namespace cv;
@@ -17,9 +20,9 @@ int main(int argc, char** argv)
     const char* filename;
     Mat input;
     Mat output;
-    Mat og_image;
-    Mat gs_image;
     int width, height, channels;
+    std::string ruta;
+    std::string operacion(argv[1]);
 
     if (argc > 0) {
         /* Comienza las llamadas a MPI */
@@ -35,43 +38,82 @@ int main(int argc, char** argv)
 
         if (procesadores < 1) {
             fprintf(stderr, "\nLa implementación requiere al menos 2 procesadores\n");
-            return 0;
+            return EXIT_FAILURE;
         }
 
         if (mi_rango == 0) {
-            // std::string operacion(argv[1]);
-            // std::string ruta(argv[2]);
-            input = imread("Fubuki.jpg");
+            ruta = argv[2];
+            input = imread(ruta);
             width = input.cols;
             height = input.rows;
             channels = input.channels();
             tam = width*height*channels;
             datosPP = height/procesadores * width * channels;
-            output = Mat(height, width, CV_8UC3);
-            // output = Mat(height/2, width/2, CV_8UC3);
+            if(argv[1] == std::string("1") || argv[1] == std::string("2")){
+                output = Mat(height, width, CV_8UC3);
+            }
+            else if(argv[1] == std::string("3")){
+                output = Mat(height*2, width*2, CV_8UC3);
+            }
+            else{
+                std::cout << "No existe una argv[1] con ese numero." << std::endl;
+                return EXIT_FAILURE;
+            }
         }
 
+        // MPI_Bcast: Envio de el mismo dato hacia todos los procesos, desde el raiz (mi_rango == 0), incluyendose.
         MPI_Bcast(&width, 1, MPI_INT, 0, MPI_COMM_WORLD);
         MPI_Bcast(&height, 1, MPI_INT, 0, MPI_COMM_WORLD);
         MPI_Bcast(&channels, 1, MPI_INT, 0, MPI_COMM_WORLD);
         MPI_Bcast(&tam, 1, MPI_INT, 0, MPI_COMM_WORLD);
         MPI_Bcast(&datosPP, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
+        // Creación de las imagenes de un tamaño de (height/procesadores) filas por procesador.
+        // MPI_Scatter: a diferencia de Bcast, separa un mensaje en partes o trozos iguales y las envia individualmente al resto de los procesos y a si mismo.
         Mat test = Mat(height/procesadores, width, CV_8UC3);
         MPI_Scatter(input.data, datosPP, MPI_BYTE, test.data, datosPP, MPI_BYTE, 0, MPI_COMM_WORLD);
         
-        GaussianBlur(test, test, Size(15, 15), 0);
-        //resize(test,test,Size(width/2,(height/procesadores)/2));
+        //Aplicación de cada operación.
+        if(argv[1] == std::string("1")){
+            GaussianBlur(test, test, Size(15, 15), 0);
+        }
+        else if(argv[1] == std::string("2")){
+            cvtColor(test, test, cv::COLOR_BGR2GRAY);
+            cvtColor(test, test, cv::COLOR_GRAY2BGR);
+        }
+        else if(argv[1] == std::string("3")){
+            resize(test,test,Size(width*2,(height/procesadores)*2));
+        }
+
+        // Se bloquea el proceso hasta que todos los procesos lo ejecuten.
         MPI_Barrier(MPI_COMM_WORLD);
 
-        MPI_Gather(test.data, datosPP, MPI_BYTE, output.data,datosPP, MPI_BYTE, 0, MPI_COMM_WORLD);
-        // MPI_Gather(test.data, datosPP / 4, MPI_BYTE, output.data,datosPP/4, MPI_BYTE, 0, MPI_COMM_WORLD);
+        // MPI_Gather: Inverso de MPI_Scatter, cada proceso envía el contenido de su buffer de salida al proceso raíz, este guarda por rango del proceso. Finalmente se reune en el proceso raiz (mi_rango == 0) el mensaje completo y ordenado.
+        // Si la argv[1] es difuminar o pasar a escala de grises, se tiene la misma cantidad de datos de entrada y salida. 
+        // En el caso 3 (escalar la imagen sin perder la calidad), se cambia la cantidad de datos de salida.
+        if(argv[1] == std::string("1") || argv[1] == std::string("2")){
+            MPI_Gather(test.data, datosPP, MPI_BYTE, output.data,datosPP, MPI_BYTE, 0, MPI_COMM_WORLD);
+        }
+        else if(argv[1] == std::string("3")){
+            MPI_Gather(test.data, datosPP*4, MPI_BYTE, output.data,datosPP*4, MPI_BYTE, 0, MPI_COMM_WORLD);
+        }
+
         MPI_Barrier(MPI_COMM_WORLD);
 
         if(mi_rango==0){
-            imwrite("elbuenblur.png", output);
+            time_t rawtime;
+            struct tm * timeinfo;
+            char buffer[80];
+
+            time (&rawtime);
+            timeinfo = localtime(&rawtime);
+
+            strftime(buffer,sizeof(buffer),"%Y%m%d%H%M%S",timeinfo);
+            std::string str(buffer);
+            std::string textoSalida = "operacion_" + operacion + "_" + str +".png";
+            imwrite(textoSalida, output);
         }
         MPI_Finalize();
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
