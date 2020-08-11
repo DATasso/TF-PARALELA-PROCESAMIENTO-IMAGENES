1785666215a97febc9ab9090ae61f637d955be2e
DATasso <50896863+DATasso@users.noreply.github.com>
2020-08-10 23:08:59 -0400

    Se añaden comentarios faltantes.

--- algo.cpp
+++ algo.cpp
@@ -1,31 +1,33 @@
-//Uncomment the following line if you are compiling this code in Visual Studio
-//#include "stdafx.h"
-
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <string.h>
-#include "mpi.h"
+#include <opencv2/opencv.hpp> // librería opencv
+#include <iostream> // STL (cout,cin,etc)
+#include <cstdlib> // biblioteca estandar EXIT_SUCCESS , EXIT_FAILURE
+#include <ctime> // libreria para obtener fecha y hora
+#include <string.h> // libreria para el manejo de strings
+#include "mpi.h" // libreria MPI
 
 using namespace cv;
 using namespace std;
+
+/**
+ * Función que muestra los participantes del grupo
+ */
+void participante();
 
 int main(int argc, char** argv)
 {
     int mi_rango; /* rango del proceso    */
     int procesadores; /* numero de procesos   */
-    int tam = 0;
-    int datosPP;
-    const char* filename;
-    Mat input;
-    Mat output;
-    Mat filasFaltantes;
-    int width, height, channels, cantFaltantes;
-    std::string ruta;
-    std::string operacion(argv[1]);
+    int tam = 0; /* tamaño total de la imagen */
+    int datosPP; /* tamaño o datos (por filas) segmentados y enviado a los procesadores */
+    Mat input; /* Imagen de entrada */
+    Mat output; /* Imagen de salida */
+    Mat filasFaltantes; /* Imagen para casos especiales de filas faltantes */
+    int width, height, channels, cantFaltantes; /* ancho(columnas), altura(filas), canales y cantidad de filas faltantes para casos especiales. */
 
-    if (argc > 0) {
+    if (argc > 2) {
+        std::string ruta; /* string con la ruta del fichero (imagen) */
+        std::string operacion(argv[1]); /* string con la operación deseada ingresada */
+
         /* Comienza las llamadas a MPI */
         MPI_Init(&argc, &argv);
 
@@ -36,15 +38,23 @@ int main(int argc, char** argv)
          * ejecutando nuestro porgrama 
          */
         MPI_Comm_size(MPI_COMM_WORLD, &procesadores);
-
-        if (procesadores < 1) {
-            fprintf(stderr, "\nLa implementación requiere al menos 2 procesadores\n");
-            return EXIT_FAILURE;
-        }
-
+        /* El procesador maestro (0), lee el archivo, obtiene los datos generales (ancho,altura,canales,tamaño) y realiza el calculo de las n filas 
+         * y cantidad de datos que se distribuiran a cada procesador y la obtención de filas faltantes, es decir, filas que no se incluyen al realizar
+         * la segmentación. Creandose una imagen con las filas faltantes.
+         */
         if (mi_rango == 0) {
+            if(operacion != std::string("1") && operacion != std::string("2") && operacion != std::string("3") ){
+                std::cout << std::endl << "No existe una operación: " << argv[1] << "." << std::endl;
+                participante();
+                return EXIT_FAILURE; // o utilizar MPI_Abort(MPI_COMM_WORLD, 1);
+            }
             ruta = argv[2];
             input = imread(ruta);
+            if (input.empty()){
+                std:: cout << std::endl << "Fallo al leer imagen, la imagen no fue encontrada.!!" << std::endl;
+                participante();
+                return EXIT_FAILURE; // o utilizar MPI_Abort(MPI_COMM_WORLD, 1);
+            }
             width = input.cols;
             height = input.rows;
             channels = input.channels();
@@ -61,15 +71,19 @@ int main(int argc, char** argv)
                     }
                 }
             }
+
+            /* Para el caso de la difuminación y escala de grises, la cantidad de datos de salida es igual a la de entrada.
+             * Se excluyen las faltantes en la distribución y se agregan al final.
+            */
             if(argv[1] == std::string("1") || argv[1] == std::string("2")){
                 output = Mat(height - height%procesadores, width, CV_8UC3);
             }
+
+            /* Para el caso del escalado, la cantidad de datos de salida es distinta (x2). 
+             * Se excluyen las faltantes en la distribución y se agregan al final.
+             */
             else if(argv[1] == std::string("3")){
                 output = Mat(height*2 - (height%procesadores)*2, width*2, CV_8UC3);
-            }
-            else{
-                std::cout << "No existe una argv[1] con ese numero." << std::endl;
-                return EXIT_FAILURE;
             }
         }
 
@@ -80,18 +94,26 @@ int main(int argc, char** argv)
         MPI_Bcast(&tam, 1, MPI_INT, 0, MPI_COMM_WORLD);
         MPI_Bcast(&datosPP, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-        // Creación de las imagenes de un tamaño de (height/procesadores) filas por procesador.
-        // MPI_Scatter: a diferencia de Bcast, separa un mensaje en partes o trozos iguales y las envia individualmente al resto de los procesos y a si mismo.
+        /* Creación de las imagenes de un tamaño de (height/procesadores) filas por procesador.
+         * MPI_Scatter: a diferencia de Bcast, separa un mensaje en partes o trozos iguales y las envia individualmente al resto de los procesos y a si mismo.
+         */
         Mat test = Mat(height/procesadores, width, CV_8UC3);
         MPI_Scatter(input.data, datosPP, MPI_BYTE, test.data, datosPP, MPI_BYTE, 0, MPI_COMM_WORLD);
         
-        //Aplicación de cada operación.
+        /* Aplicación de difuminación gaussiana de cada segmento de imagen por procesador, función GaussianBlur() de openCV.
+         * Se incluye la aplicación del filtro a las filas faltantes, en caso de que la segmentación excluya height%procesadores filas.
+         */
         if(argv[1] == std::string("1")){
             GaussianBlur(test, test, Size(15, 15), 0);
             if(mi_rango == 0 && (height%procesadores != 0)){
                 GaussianBlur(filasFaltantes, filasFaltantes, Size(15, 15), 0);
             }
         }
+
+        /* Aplicación de escala a grises de cada segmento de imagen por procesador, función cvtColor() de openCV.
+         * Al pasar a grayscale, los canales pasan a ser uno solo, por lo que es necesario volver a BGR mediante para conservar el tamaño y cantidad de pixeles del archivo.
+         * Se incluye la aplicación del filtro a las filas faltantes, en caso de que la segmentación excluya height%procesadores filas.
+        */
         else if(argv[1] == std::string("2")){
             cvtColor(test, test, cv::COLOR_BGR2GRAY);
             cvtColor(test, test, cv::COLOR_GRAY2BGR);
@@ -100,6 +122,11 @@ int main(int argc, char** argv)
                 cvtColor(filasFaltantes, filasFaltantes, cv::COLOR_GRAY2BGR);
             }
         }
+
+        /*
+            Aplicación de escalado*2 de cada segmento de imagen por procesador, función resize() de openCV.
+            Se incluye la aplicación del filtro a las filas faltantes, en caso de que la segmentación excluya height%procesadores filas.
+        */
         else if(argv[1] == std::string("3")){
             resize(test,test,Size(width*2,(height/procesadores)*2));
             if(mi_rango == 0 && (height%procesadores != 0)){
@@ -110,9 +137,11 @@ int main(int argc, char** argv)
         // Se bloquea el proceso hasta que todos los procesos lo ejecuten.
         MPI_Barrier(MPI_COMM_WORLD);
 
-        // MPI_Gather: Inverso de MPI_Scatter, cada proceso envía el contenido de su buffer de salida al proceso raíz, este guarda por rango del proceso. Finalmente se reune en el proceso raiz (mi_rango == 0) el mensaje completo y ordenado.
-        // Si la argv[1] es difuminar o pasar a escala de grises, se tiene la misma cantidad de datos de entrada y salida. 
-        // En el caso 3 (escalar la imagen sin perder la calidad), se cambia la cantidad de datos de salida.
+        /* 
+            MPI_Gather: Inverso de MPI_Scatter, cada proceso envía el contenido de su buffer de salida al proceso raíz, este guarda por rango del proceso. Finalmente se reune en el proceso raiz (mi_rango == 0) el mensaje completo y ordenado.
+            Si la argv[1] es difuminar o pasar a escala de grises, se tiene la misma cantidad de datos de entrada y salida. 
+            En el caso 3 (escalar la imagen sin perder la calidad), se cambia la cantidad de datos de salida.
+        */
         if(argv[1] == std::string("1") || argv[1] == std::string("2")){
             MPI_Gather(test.data, datosPP, MPI_BYTE, output.data,datosPP, MPI_BYTE, 0, MPI_COMM_WORLD);
         }
@@ -122,6 +151,9 @@ int main(int argc, char** argv)
 
         MPI_Barrier(MPI_COMM_WORLD);
 
+        /* Finalmente, en el rango 0, se obtiene la fecha y hora actual, para luego obtener un string operacion_numeroDeOperación_yyyyymmddhhmmss.png
+         * Se concatenan las filas faltantes en output para obtener imagen final. Funcion vconcat() de opencv.
+         */
         if(mi_rango==0){
             time_t rawtime;
             struct tm * timeinfo;
@@ -139,8 +171,15 @@ int main(int argc, char** argv)
             }
 
             imwrite(textoSalida, output);
+            std:: cout << std::endl <<"Se creo correctamente la imagen: " << textoSalida << std:: endl;
+            participante();
         }
         MPI_Finalize();
     }
     return EXIT_SUCCESS;
+}
+
+void participante() {
+    std::cout << std::endl << "=== Realizado por ===" << std::endl;
+    std::cout << "- Daniel Aguilera Tasso" << std::endl << "- Nicolas Andrews Sandoval" << std::endl;
 }
